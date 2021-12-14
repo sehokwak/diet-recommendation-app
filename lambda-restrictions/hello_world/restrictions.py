c9d1613d7fb05d021e983f51020850fd6660a49a
Se Ho Kwak <skai2018@mymail.pomona.edu>
2021-12-14 22:34:07 +0900

    clean up

--- lambda-restrictions/hello_world/restrictions.py
+++ lambda-restrictions/hello_world/restrictions.py
@@ -1,5 +1,4 @@
 import json
-import getRecipes
 
 
 import boto3
@@ -34,82 +33,3 @@ def post_handler(event, context):
     }
 
 
-# PUT updates existing entries in database
-def put_handler(event, context):
-    nickname = event['nickname']
-    restrictions = event['restrictions']
-
-    response = table.update_item(
-        Key={
-            "id":nickname,
-        },
-        UpdateExpression='set restrictions = :r',
-        ExpressionAttributeValues={
-            ':r': restrictions
-        })
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": f"hello {nickname}, your restrictions have been updated to {restrictions}"
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
-
-
-# GET reads item in database
-def get_handler(event, context):
-    nickname = event['params']['nickname']
-    cuisine = event['params']['cuisine']
-
-    response = table.get_item(
-        Key={
-            "id":nickname
-        })
-
-    
-    recipes = getRecipes.byIngredients(response['Item'], cuisine)
-
-    # try:
-    #     response = table.get_item(
-    #         Key={
-    #             "id":nickname,
-    #         })
-    # except ClientError as e:
-    #     print(e.response['Error']['Message'])
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(recipes[0]['title']
-        # {
-        #     "message": f"hello {nickname}, your restrictions stored in the database are {response['Item']}"
-            
-        # }
-        )
-    }
-
-
-def get_recipes(event, context):
-    id = event['nickname']
-    cuisine = event['cuisine']
-
-    print('before getting item')
-    response = table.get_item(
-        Key={
-            'id':id
-        })
-
-    
-    print(f'reached {response}')
-
-    recipes = getRecipes.byIngredients(response['Item'], cuisine)
-
-    titles = []
-    for i in range(len(recipes)):
-        titles.append(recipes[i]['title'])
-
-    print(titles)
-    return {
-        "statusCode": 200,
-        "body": json.dumps(titles)
-    }
